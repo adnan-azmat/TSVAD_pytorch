f2fef47ceb9ba4f93fdede0dec2640cca96d75b8
#AZMAT ADNAN# <adnan002@a2ap-login02.asp2p.nscc.sg>
2025-02-25 14:40:57 +0800

    hyperparams

--- ts-vad/run_eval.sh
+++ ts-vad/run_eval.sh
@@ -1,12 +1,14 @@
-DATA_PATH="/home/users/ntu/adnan002/scratch/data/DIHARD3_denoise"
-OUTPUT_PATH="exps/d8_denoise_ft_model_eval_wav_denoise"
-MODEL_PATH="/home/users/ntu/adnan002/scratch/repos/Adnan/2/TSVAD_pytorch/ts-vad/pretrained_models/model_0019_finetuned.model"
+DATA_PATH="/home/users/ntu/adnan002/scratch/data/klass_v2/recorded_corpusV2/single"
+OUTPUT_PATH="exps/eval_klass_v2_4spk_amitrain"
+# MODEL_PATH="/home/users/ntu/adnan002/scratch/repos/Adnan/2/TSVAD_pytorch/ts-vad/pretrained_models/model_0019_finetuned.model"
+
+MODEL_PATH="/home/users/ntu/adnan002/scratch/repos/Adnan/2/TSVAD_pytorch/ts-vad/exps/p418_voxconversedev17_voxconversetest8_msd5_amitrain/model/model_0012.model"
 
 python main.py \
-    --train_list ${DATA_PATH}/third_dihard_challenge_dev/data/ts_infer.json \
-    --train_path ${DATA_PATH}/third_dihard_challenge_dev/data \
-    --eval_list ${DATA_PATH}/third_dihard_challenge_eval/data/ts_infer.json \
-    --eval_path ${DATA_PATH}/third_dihard_challenge_eval/data \
+    --train_list ${DATA_PATH}/ts_infer.json \
+    --train_path ${DATA_PATH} \
+    --eval_list ${DATA_PATH}/ts_infer.json \
+    --eval_path ${DATA_PATH} \
     --musan_path /home/users/ntu/adnan002/scratch/data/augment/musan \
     --rir_path /home/users/ntu/adnan002/scratch/data/augment/RIRS_NOISES/simulated_rirs \
     --save_path ${OUTPUT_PATH} \
@@ -18,4 +20,4 @@ python main.py \
     --n_cpu 14 \
     --eval \
     --init_model ${MODEL_PATH} \
-    --max_speaker 8
+    --max_speaker 4
